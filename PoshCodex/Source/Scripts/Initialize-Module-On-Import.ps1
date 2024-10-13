f6f7bf11d9426875723f6167f974a8a4bc191900
Rishikesh Rachchh <rishikeshrachchh@gmail.com>
2024-10-13 12:13:21 +0530

    🎨 Formatting

--- PoshCodex/Source/Scripts/Initialize-Module-On-Import.ps1
+++ PoshCodex/Source/Scripts/Initialize-Module-On-Import.ps1
@@ -7,7 +7,8 @@
 if (-not [Environment]::GetEnvironmentVariable('AUTOCOMPLETE_KEYBIND', 'User')) {
 	## Use default keybind, if none is set
 	$default_keybind = 'Ctrl+Shift+O'
-} else {
+}
+else {
 	## Use existing keybind
 	$default_keybind = [Environment]::GetEnvironmentVariable('AUTOCOMPLETE_KEYBIND', 'User')
 }
