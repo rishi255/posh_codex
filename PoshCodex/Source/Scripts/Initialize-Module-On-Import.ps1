ac9f99cc04610624e09378d7d4eaaa1ef91963b6
Kris Borowinski <kris.borowinski@gmail.com>
2024-10-13 02:15:48 +0200

    Make sure to call Set-CompletionKeybind

--- PoshCodex/Source/Scripts/Initialize-Module-On-Import.ps1
+++ PoshCodex/Source/Scripts/Initialize-Module-On-Import.ps1
@@ -4,9 +4,11 @@
 [Environment]::SetEnvironmentVariable('OLLAMA_MODEL', 'rishi255/posh_codex_model', [System.EnvironmentVariableTarget]::User)
 
 
-if (-not [Environment]::GetEnvironmentVariable('AUTOCOMPLETE_KEYBIND', 'User') {
-  ## Set default keybind, if not already set:
-  
-  $default_keybind = 'Ctrl+Shift+O'
-  Set-CompletionKeybind $null $default_keybind;
+if (-not [Environment]::GetEnvironmentVariable('AUTOCOMPLETE_KEYBIND', 'User')) {
+	## Use default keybind, if none is set
+	$default_keybind = 'Ctrl+Shift+O'
+} else {
+	## Use existing keybind
+	$default_keybind = [Environment]::GetEnvironmentVariable('AUTOCOMPLETE_KEYBIND', 'User')
 }
+Set-CompletionKeybind $null $default_keybind
